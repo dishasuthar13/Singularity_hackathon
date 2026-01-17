5e6a9f7b744d8aadd4e4af2f81739aa059c29c52
dishasuthar13 <suthardisha354@gmail.com>
2026-01-17 13:01:45 +0530

    Add files via upload

---
+++ SheSecureApplication.java
@@ -0,0 +1,13 @@
+package com.shesecure.she_secure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SheSecureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SheSecureApplication.class, args);
+	}
+
+}
